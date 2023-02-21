9b052f3ccf2e484e9b1e9d0dcf15f3f2875c25ca
Sddilora <sumeyyedilaradogan@gmail.com>
2023-02-21 15:43:44 +0300

    some of the functions get fixed and func tests completed with succes

--- converters/converters.go
+++ converters/converters.go
@@ -1,7 +1,6 @@
 package converters
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -87,15 +86,16 @@ func BinaryToOctal(input string) string { // Binary to Octal
 
 	input = string(inputholder)
 
-	for i := 0; i < len(input); i += 3 { // Her 3 karakter için sekizli sayıları birleştiriyoruz
+	for i := 0; i < len(input); i += 8 {
 
-		if i+3 > len(input) { // Eğer son 3 karakterin uzunluğu 3'ten az ise başına sıfır ekliyoruz
-			input += strings.Repeat("0", i+3-len(input))
+		if i+3 > len(input) {
+			input += strings.Repeat("0", i+8-len(input))
 		}
 
-		binary := string(input[i : i+3]) // 3 bitlik ikili sayıyı sekizli sayıya dönüştürüyoruz
+		binary := input[i : i+8]
 		octal, _ := strconv.ParseInt(binary, 2, 64)
 		output += strconv.FormatInt(octal, 8) + " "
+
 	}
 
 	return output
@@ -115,13 +115,13 @@ func OctalToAscii(input string) string { // Octal to ASCII
 
 	input = string(inputholder)
 
-	for i := 0; i < len(input); i += 2 { // Her 2 karakter için ASCII karakterlerini birleştiriyoruz
+	for i := 0; i < len(input); i += 3 { // Her 3 karakter için ASCII karakterlerini birleştiriyoruz
 
-		if i+2 > len(input) { // Eğer son 2 karakterin uzunluğu 2'den az ise başına sıfır ekliyoruz
-			input += strings.Repeat("0", i+2-len(input))
+		if i+3 > len(input) { // Eğer son 3 karakterin uzunluğu 2'den az ise başına sıfır ekliyoruz
+			input += strings.Repeat("0", i+3-len(input))
 		}
 
-		octal := input[i : i+2] // 2 basamaklı sekizli sayıyı ASCII karakterine dönüştürüyoruz
+		octal := input[i : i+3] // 3 basamaklı sekizli sayıyı ASCII karakterine dönüştürüyoruz
 		ascii, _ := strconv.ParseInt(octal, 8, 64)
 		output += string(ascii) + " "
 	}
@@ -143,17 +143,26 @@ func OctalToBinary(input string) string { // Octal to Binary
 
 	input = string(inputholder)
 
-	for _, c := range input { // Her 1 karakter için 3 bitlik ikili sayıları birleştiriyoruz
+	for i := 0; i < len(input); i += 3 { // Her 3 karakter için ASCII karakterlerini birleştiriyoruz
 
-		octal := string(c) // Sekizli sayıyı 3 bitlik ikili sayıya dönüştürüyoruz
-		binary := strconv.FormatInt(int64(octal[0]-'0'), 2)
-		for i := 1; i < len(octal); i++ {
-			digit := octal[i] - '0'
-			padded := fmt.Sprintf("%03d", digit)
-			binary += padded
+		if i+3 > len(input) { // Eğer son 3 karakterin uzunluğu 2'den az ise başına sıfır ekliyoruz
+			input += strings.Repeat("0", i+3-len(input))
 		}
 
-		output += binary + " " // 3 bitlik ikili sayıları birleştiriyoruz
+		octal := input[i : i+3] // 3 basamaklı sekizli sayıyı ASCII karakterine dönüştürüyoruz
+		ascii, _ := strconv.ParseInt(octal, 8, 64)
+		for _, c := range string(ascii) {
+
+			binary := strconv.FormatInt(int64(c), 2) // ASCII kodunu ikili sayıya çeviriyoruz
+
+			if len(binary) < 8 { // Eğer sayının uzunluğu 8 bit değilse başına sıfır ekliyoruz
+				for i := len(binary); i < 8; i++ {
+					binary = "0" + binary
+				}
+			}
+
+			output += binary + " " // Binary çıktısını birleştiriyoruz
+		}
 	}
 
 	return output
--- tests/converters_test.go
+++ tests/converters_test.go
@@ -18,3 +18,72 @@ func TestAsciiToBinary(t *testing.T) {
 		}
 	}
 }
+
+func TestAsciiToOctal(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"dilara", "144 151 154 141 162 141 "},
+	}
+	for _, tc := range testcases {
+		res := converters.AsciiToOctal(tc.in)
+		if res != tc.want {
+			t.Errorf("sonuç: %s, olması gereken: %s", res, tc.want)
+		}
+	}
+}
+
+func TestBinaryToAscii(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"01100100 01101001 01101100 01100001 01110010 01100001", "d i l a r a "},
+	}
+	for _, tc := range testcases {
+		res := converters.BinaryToAscii(tc.in)
+		if res != tc.want {
+			t.Errorf("sonuç: %s, olması gereken: %s", res, tc.want)
+		}
+	}
+}
+func TestBinaryToOctal(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"01100100 01101001 01101100 01100001 01110010 01100001", "144 151 154 141 162 141 "},
+	}
+	for _, tc := range testcases {
+		res := converters.BinaryToOctal(tc.in)
+		if res != tc.want {
+			t.Errorf("sonuç: %s, olması gereken: %s", res, tc.want)
+		}
+	}
+}
+
+func TestOctaltoAscii(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"144 151 154 141 162 141", "d i l a r a "},
+	}
+	for _, tc := range testcases {
+		res := converters.OctalToAscii(tc.in)
+		if res != tc.want {
+			t.Errorf("sonuç: %s, olması gereken: %s", res, tc.want)
+		}
+	}
+}
+
+func TestOctaltoBinary(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"144 151 154 141 162 141", "01100100 01101001 01101100 01100001 01110010 01100001 "},
+	}
+	for _, tc := range testcases {
+		res := converters.OctalToBinary(tc.in)
+		if res != tc.want {
+			t.Errorf("sonuç: %s, olması gereken: %s", res, tc.want)
+		}
+	}
+}
